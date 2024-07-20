00502118cc218de4dbd6825687c2366ab20366ca
PXIFusionX <barron.samuel111@gmail.com>
2024-07-20 11:23:29 -0700

    Csp

---
+++ background.js
@@ -0,0 +1,24 @@
+let isEnabled = false;
+
+chrome.action.onClicked.addListener((tab) => {
+  isEnabled = !isEnabled;
+  chrome.storage.local.set({ isEnabled });
+  if (isEnabled) {
+    chrome.webRequest.onHeadersReceived.addListener(
+      removeCSPHeaders,
+      { urls: ["<all_urls>"] },
+      ["blocking", "responseHeaders"]
+    );
+  } else {
+    chrome.webRequest.onHeadersReceived.removeListener(removeCSPHeaders);
+  }
+  chrome.action.setIcon({
+    path: isEnabled ? "icons/icon128_enabled.png" : "icons/icon128.png",
+    tabId: tab.id
+  });
+});
+
+function removeCSPHeaders(details) {
+  details.responseHeaders = details.responseHeaders.filter(header => header.name.toLowerCase() !== 'content-security-policy');
+  return { responseHeaders: details.responseHeaders };
+}
